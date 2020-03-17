d438b8743627742f575966e168628ec21f8e8ed9
tawfiq <tawfiq.benayad@gmail.com>
2020-03-17 17:04:29 +0100

    first commit : ajout de la classe ServletInitializer

---
+++ src/main/java/com/ben/library/library/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ben.library.library;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LibraryApplication.class);
+	}
+
+}
